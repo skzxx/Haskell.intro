649f89b5135c05bd5f25f2ce38fd8b177a54649e
Elon Reeve Musk <76138584+skzxx@users.noreply.github.com>
2021-08-13 20:03:49 +0700

    Create Test.vb

---
+++ Test.vb
@@ -0,0 +1,13 @@
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text.RegularExpressions
+
+Namespace Dcoder
+  Public Module Program
+    Public Sub Main(args() As string)
+      'Your code goes here
+      Console.WriteLine("Hello, Dcoder!")
+    End Sub
+  End Module
+End Namespace
